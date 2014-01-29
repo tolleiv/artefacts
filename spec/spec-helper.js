2be9d876a02db5f13fb58827106eb28cf7188726
Tolleiv Nietsch <info@tolleiv.de>
2014-01-29 07:45:05 +0100

    Cleanup the spe-chelper - port is not needed there

--- spec/spec-helper.js
+++ spec/spec-helper.js
@@ -2,26 +2,26 @@ var Requester, request;
 var http = require('http');
 request = require("request");
 
-var server, app, started=false, port=3000;
+var server, app, started=false;
 
 exports.req = {
     get: function (path, callback) {
-        return request("http://127.0.0.1:" + port + path, callback);
+        return request("http://127.0.0.1:3000" + path, callback);
     },
     post: function (path, data, callback) {
         return request.post({
-            url: "http://127.0.0.1:" + port + path,
+            url: "http://127.0.0.1:3000" + path,
             json: data
         }, callback);
     },
     put: function (path, data, callback) {
         return request.put({
-            url: "http://127.0.0.1:" + port + path,
+            url: "http://127.0.0.1:3000" + path,
             json: data
         }, callback);
     },
     del: function(path, callback) {
-        return request.del("http://127.0.0.1:" + port + path, callback);
+        return request.del("http://127.0.0.1:3000" + path, callback);
     }
 };
 
@@ -43,7 +43,7 @@ var startServer = function (env, model, conn) {
     app.set('model', model);
     app.set('connection', conn)
     server = http.createServer(app);
-    server.listen(port)
+    server.listen(3000)
         .on('listening', function () {
             setTimeout(function() { started=true; }, 100)
         })
@@ -51,7 +51,6 @@ var startServer = function (env, model, conn) {
 };
 
 exports.start = function(persist, models, objects) {
-    port = port + Math.round(Math.random()*50)
     return function() {
         persist.env = 'dev';
         createModel(persist, objects, function(conn) {
